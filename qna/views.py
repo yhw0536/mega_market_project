97987b9553fcbe9459227ed0c4392b3a7ed027a4
yhw0536 <yhw0536@gmail.com>
2021-12-22 21:14:53 +0900

    선í택된 상품에 관련된 질문이 생성

--- qna/views.py
+++ qna/views.py
@@ -1,16 +1,18 @@
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from products.models import Product
 from qna.models import Question
 
 
-def create_question(request: HttpRequest):
-    product_id = 1
-    product = Product.objects.get(id=product_id)
-    product_content_type = ContentType.objects.get_for_model(product)
-    body = request.POST.get("body")
-    question = Question(user_id=1, content_type=product_content_type, object_id=product_id, body=body)
-    question.save()
+def create_question(request: HttpRequest, content_type, object_id):
+    product_id = object_id
+    if content_type == "product":
+        product = Product.objects.get(id=product_id)
+        product_content_type = ContentType.objects.get_for_model(product)
+        body = request.POST.get("body")
+        question = Question(user_id=1, content_type=product_content_type, object_id=product_id, body=body)
+        question.save()
+        return redirect("products:detail", id=product_id)
     return HttpResponse("성공")
