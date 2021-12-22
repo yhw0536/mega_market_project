527a83ee6bb5551f2cfcf480950b88003f5574b1
yhw0536 <yhw0536@gmail.com>
2021-12-22 17:11:19 +0900

    DB 연결

--- base/settings/common.py
+++ base/settings/common.py
@@ -86,8 +86,15 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mega_market',
+        'USER': 'sbsst',
+        'PASSWORD': 'sbs123414',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", # 추가, 만약에 이 부분 때문에 오류가 난다면, 이 라인을 지우고 다시 시도해주세요.
+        },
     }
 }
 
