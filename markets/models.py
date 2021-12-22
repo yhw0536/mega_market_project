c3e53794974f0e476af83bf38c410861e6db1111
yhw0536 <yhw0536@gmail.com>
2021-12-22 17:40:48 +0900

    Market Model

--- markets/models.py
+++ markets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from accounts.models import User
+
+
+class Market(models.Model):
+    reg_date = models.DateTimeField('등록날짜', auto_now_add=True)
+    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    name = models.CharField('마켓이름', max_length=100)
+    site_url = models.URLField('마켓사이트URL', max_length=100)
+    email = models.EmailField('마켓대표이메일', max_length=100)
+    master = models.OneToOneField(User, on_delete=models.CASCADE)
