7058264d77c6f3a96ba9851059f46973fee228c0
yhw0536 <yhw0536@gmail.com>
2021-12-22 21:00:57 +0900

    질문 저장 성공

--- qna/views.py
+++ qna/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpRequest
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-def create_question(request:HttpRequest):
-    return HttpRequest("hi")
+from products.models import Product
+from qna.models import Question
+
+
+def create_question(request: HttpRequest):
+    product_id = 1
+    product = Product.objects.get(id=product_id)
+    product_content_type = ContentType.objects.get_for_model(product)
+    body = request.POST.get("body")
+    question = Question(user_id=1, content_type=product_content_type, object_id=product_id, body=body)
+    question.save()
+    return HttpResponse("성공")
