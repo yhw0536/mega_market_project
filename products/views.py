2b3eb1a5207c2afad74f1b6f28515f8cbe34b020
yhw0536 <yhw0536@gmail.com>
2021-12-22 20:31:12 +0900

    상품 상ì상세에서 옵션 노출

--- products/views.py
+++ products/views.py
@@ -4,15 +4,18 @@ from products.models import Product
 
 
 def product_list(request: HttpRequest):
-    product_list = Product.objects.order_by('-id')
+    products = Product.objects.order_by('-id')
     return render(request, "products/product_list.html", {
-        "product_list": product_list
+        "products": products
     })
 
 
 def product_detail(request: HttpRequest, id):
     product = get_object_or_404(Product, id=id)
 
+    product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
+
     return render(request, "products/product_detail.html", {
-        "product": product
+        "product": product,
+        "product_reals": product_reals
     })
