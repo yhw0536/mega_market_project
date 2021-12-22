a7e3d67467f4f87bf8d129683f81fd8dfd4a82fc
yhw0536 <yhw0536@gmail.com>
2021-12-22 18:38:39 +0900

    질답 모델

--- qna/models.py
+++ qna/models.py
@@ -1,3 +1,25 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 # Create your models here.
+from accounts.models import User
+
+
+class Question(models.Model):
+    reg_date = models.DateTimeField('등록날짜', auto_now_add=True)
+    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, related_name="content_type_question", on_delete=models.DO_NOTHING)
+    object_id = models.PositiveIntegerField('관련 데이터 번호')
+    content_object = GenericForeignKey('content_type', 'object_id')
+    body = models.TextField('내용')
+    is_complete = models.BooleanField('답변완료여부', default=False)
+
+
+class Answer(models.Model):
+    reg_date = models.DateTimeField('등록날짜', auto_now_add=True)
+    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField('내용')
