bf7d022b47ec4bca531e5a446529b00665f178c5
yhw0536 <yhw0536@gmail.com>
2021-12-23 17:22:14 +0900

    질문삭제

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.product_list, name='list'),
-    path('<int:id>/', views.product_detail, name='detail'),
-    path('<int:id>/question/create/', views.question_create, name='question_create'),
+    path('<int:product_id>/', views.product_detail, name='detail'),
+    path('<int:product_id>/question/create/', views.question_create, name='question_create'),
+    path('<int:product_id>/question/delete/<int:question_id>', views.question_delete, name='question_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
 from qna.forms import QuestionForm
+from qna.models import Question
 
 
 def product_list(request: HttpRequest):
@@ -12,8 +13,8 @@ def product_list(request: HttpRequest):
         "products": products
     })
 
-def _product_detail(request: HttpRequest, id):
-    product = get_object_or_404(Product, id=id)
+def _product_detail(request: HttpRequest, product_id):
+    product = get_object_or_404(Product, id=product_id)
 
     if request.method == "POST":
         form = QuestionForm(request.POST)
@@ -24,11 +25,14 @@ def _product_detail(request: HttpRequest, id):
             question.user_id = 1
             question.save()
             messages.success(request, "질문이 등록되었습니다.")
-            return redirect("products:detail", id=product.id)
+
+            return redirect("products:detail", product_id=product.id)
     else:
         form = QuestionForm()
+
     product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
     questions = product.question.order_by('-id')
+
     return render(request, "products/product_detail.html", {
         "product": product,
         "product_reals": product_reals,
@@ -36,9 +40,20 @@ def _product_detail(request: HttpRequest, id):
         "question_form": form
     })
 
-def product_detail(request: HttpRequest, id):
-    return _product_detail(request, id)
+
+def product_detail(request: HttpRequest, product_id):
+    return _product_detail(request, product_id)
 
 
-def question_create(request:HttpRequest, id):
-    return _product_detail(request, id)
+def question_create(request:HttpRequest, product_id):
+    return _product_detail(request, product_id)
+
+
+def question_delete(request: HttpRequest, product_id, question_id):
+    question = get_object_or_404(Question, id=question_id)
+
+    question.delete()
+
+    messages.success(request, "질문이 삭제되었습니다.")
+
+    return redirect("products:detail", product_id=product_id)
