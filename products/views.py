8eeadf045debe7effa30e71d3f0be0cdfe4df2ed
yhw0536 <yhw0536@gmail.com>
2021-12-23 13:18:45 +0900

    질문 등록 성공 후 메시지보여줌

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
+from qna.models import Question
 
 
 def product_list(request: HttpRequest):
@@ -20,8 +23,19 @@ def product_detail(request: HttpRequest, id):
         "product_reals": product_reals
     })
 
+
 def question_create(request: HttpRequest, id):
     product = get_object_or_404(Product, id=id)
+
+    if request.method == "POST":
+        product_content_type = ContentType.objects.get_for_model(product)
+        body = request.POST.get("body")
+        question = Question(user_id=1, content_type=product_content_type, object_id=product.id, body=body)
+        question.save()
+
+        messages.success(request, "질문이 등록되었습니다.")
+
+        return redirect("products:detail", id=product.id)
 
     product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
 
