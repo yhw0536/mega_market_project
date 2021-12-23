3143e59db25c0e9cb31338dd23dc65d5d6939a5a
yhw0536 <yhw0536@gmail.com>
2021-12-23 13:40:48 +0900

    QuestionForm 으로 Form 작업

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
+from qna.forms import QuestionForm
 from qna.models import Question
 
 
@@ -18,9 +19,12 @@ def product_detail(request: HttpRequest, id):
 
     product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
 
+    form = QuestionForm()
+
     return render(request, "products/product_detail.html", {
         "product": product,
-        "product_reals": product_reals
+        "product_reals": product_reals,
+        "question_form": form,
     })
 
 
@@ -28,18 +32,22 @@ def question_create(request: HttpRequest, id):
     product = get_object_or_404(Product, id=id)
 
     if request.method == "POST":
-        product_content_type = ContentType.objects.get_for_model(product)
-        body = request.POST.get("body")
-        question = Question(user_id=1, content_type=product_content_type, object_id=product.id, body=body)
-        question.save()
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.content_type = ContentType.objects.get_for_model(product)
+            question.object_id = product.id
+            question.user_id = 1
+            question.save()
+            messages.success(request, "질문이 등록되었습니다.")
 
-        messages.success(request, "질문이 등록되었습니다.")
-
-        return redirect("products:detail", id=product.id)
-
+            return redirect("products:detail", id=product.id)
+    else:
+        form = QuestionForm()
     product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
 
     return render(request, "products/product_detail.html", {
         "product": product,
-        "product_reals": product_reals
+        "product_reals": product_reals,
+        "question_form": form,
     })
