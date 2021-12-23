21e4ed9d8b37f29249c24ef86c168811f207d6fc
yhw0536 <yhw0536@gmail.com>
2021-12-23 16:55:55 +0900

    product detail과 question create간의 중복 소스코드 제거

--- products/views.py
+++ products/views.py
@@ -1,10 +1,9 @@
 from django.contrib import messages
 from django.contrib.contenttypes.models import ContentType
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
 from qna.forms import QuestionForm
-from qna.models import Question
 
 
 def product_list(request: HttpRequest):
@@ -13,22 +12,7 @@ def product_list(request: HttpRequest):
         "products": products
     })
 
-
-def product_detail(request: HttpRequest, id):
-    product = get_object_or_404(Product, id=id)
-
-    product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
-
-    form = QuestionForm()
-
-    return render(request, "products/product_detail.html", {
-        "product": product,
-        "product_reals": product_reals,
-        "question_form": form,
-    })
-
-
-def question_create(request: HttpRequest, id):
+def _product_detail(request: HttpRequest, id):
     product = get_object_or_404(Product, id=id)
 
     if request.method == "POST":
@@ -40,14 +24,19 @@ def question_create(request: HttpRequest, id):
             question.user_id = 1
             question.save()
             messages.success(request, "질문이 등록되었습니다.")
-
             return redirect("products:detail", id=product.id)
     else:
         form = QuestionForm()
     product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
-
     return render(request, "products/product_detail.html", {
         "product": product,
         "product_reals": product_reals,
-        "question_form": form,
+        "question_form": form
     })
+
+def product_detail(request: HttpRequest, id):
+    return _product_detail(request, id)
+
+
+def question_create(request:HttpRequest, id):
+    return _product_detail(request, id)
