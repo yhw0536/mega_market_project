cc26a8cfe6748c9d034e124f0698ec76baf86a71
yhw0536 <yhw0536@gmail.com>
2021-12-24 12:53:53 +0900

    회원가입, 로그아웃

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.views import logout_then_login
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
 
-# Create your views here.
+from .forms import SignupForm
+
+
+def signout(request: HttpRequest):
+    messages.success(request, "로그아웃 되었습니다.")
+    return logout_then_login(request)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            signed_user = form.save()
+            auth_login(request, signed_user)
+            messages.success(request, "회원가입을 환영합니다.")
+            # signed_user.send_welcome_email()
+            next_url = request.GET.get('next', '/')
+            return redirect(next_url)
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
--- base/urls.py
+++ base/urls.py
@@ -22,6 +22,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
     path('qna/', include('qna.urls')),
     path('admin/', admin.site.urls),
