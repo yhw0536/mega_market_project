fc23c718df08d4fc04af27be547216afa9f93395
yhw0536 <yhw0536@gmail.com>
2022-01-04 12:22:07 +0900

    장바구니 품목 모델 생성

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from accounts.models import User
+from products.models import ProductReal
+
+
+class CartItem(models.Model):
+    reg_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_real = models.ForeignKey(ProductReal, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
